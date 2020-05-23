7e59649e7c8d04730c2444204978dea3374bdcfb
Immanuel Bayu <immanuelbayu@Immanuels-MacBook-Pro.local>
2020-05-23 23:51:10 +0700

    Initial Commit

---
+++ src/main/java/com/immanuelbayu/my/id/project/ProjectWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.immanuelbayu.my.id.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immanuelbayu/my/id/project/ProjectWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immanuelbayu.my.id.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
